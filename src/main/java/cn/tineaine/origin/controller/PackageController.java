8225b53f0d40dd68b2c98e4c9ca722919d448ad4
Aine <Tsos201@outlook.com>
2022-08-24 14:13:54 +0800

    后台修改

--- src/main/java/cn/tineaine/origin/controller/PackageController.java
+++ src/main/java/cn/tineaine/origin/controller/PackageController.java
@@ -1,4 +1,15 @@
 package cn.tineaine.origin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("package")
 public class PackageController {
+
+    @GetMapping("/getList")
+    public <T> T getList() {
+        return (T) "package/list";
+    }
+
 }
