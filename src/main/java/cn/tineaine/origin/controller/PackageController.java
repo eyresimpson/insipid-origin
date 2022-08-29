9791b42b9bcc3904b953509a64ade8f6f20b3c77
Aine <Tsos201@outlook.com>
2022-08-29 16:20:44 +0800

    增加Hutools依赖，便于操作JSON 规划API结构

--- src/main/java/cn/tineaine/origin/controller/PackageController.java
+++ src/main/java/cn/tineaine/origin/controller/PackageController.java
@@ -1,15 +1,52 @@
 package cn.tineaine.origin.controller;
 
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("package")
+@RestController
+@RequestMapping(value = "app")
 public class PackageController {
-
-    @GetMapping("/getList")
+    // 获取应用列表
+    @GetMapping("/list")
     public <T> T getList() {
-        return (T) "package/list";
+//        {
+        //   name,
+        //   type: "script",
+        //   description: "一个简单的脚本",
+        //   author: "aine",
+        //   updateTime: new Date(),
+        //   downloadCount: 0,
+        //   grade: 0,
+        //   picture: [],
+        //   size: "1220kb",
+        //   language: "zh-CN",
+        //   cost: "0￥",
+        //   state: "uninstalled",
+        // }
+        JSONArray arr = new JSONArray();
+        JSONObject obj = JSONUtil.parseObj(
+                "{" +
+                        "name:\"test\"," +
+                        "type: \"script\"," +
+                        "description: \"非常好\"," +
+                        "author: \"aine\"," +
+                        "updateTime: new Date()," +
+                        "downloadCount: 0," +
+                        "grade: 0," +
+                        "picture: []," +
+                        "size: \"1220kb\"," +
+                        "language: \"zh-CN\"," +
+                        "cost: \"0￥\"," +
+                        "state: \"uninstalled\"," +
+                        "}"
+        );
+        arr.put(obj);
+        return (T) arr;
     }
 
 }
