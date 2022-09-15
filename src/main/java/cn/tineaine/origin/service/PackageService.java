5107a7699b0094c0e7e2260ae33060b81f6d6de6
Aine <Tsos201@outlook.com>
2022-09-15 09:12:52 +0800

    Fixing bug

--- src/main/java/cn/tineaine/origin/service/PackageService.java
+++ src/main/java/cn/tineaine/origin/service/PackageService.java
@@ -2,7 +2,6 @@ package cn.tineaine.origin.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
 public interface PackageService {
     void getPackageList();
 }
--- src/main/java/cn/tineaine/origin/service/impl/PackageServiceImpl.java
+++ src/main/java/cn/tineaine/origin/service/impl/PackageServiceImpl.java
@@ -1,7 +1,9 @@
 package cn.tineaine.origin.service.impl;
 
 import cn.tineaine.origin.service.PackageService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PackageServiceImpl implements PackageService {
     @Override
     public void getPackageList() {
